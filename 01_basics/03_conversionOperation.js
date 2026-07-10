07e7c2e9c13c40b315b651c8bf09cdd3b4671dfa
Shivani Singh <150514536+shivani764300@users.noreply.github.com>
2026-07-10 03:07:06 +0000

    conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,7 +12,7 @@ let valueInNumber = Number(score) // ye "33" is String ko --> number change ke l
 console.log(typeof valueInNumber); // ab ham iska use kar ke  - jo value ko conversion kiya hai uska typeof check karte hai  Example - let spoose - "33abc" ye ek String hai but isko ham number mein convert kiye hai .
 console.log(valueInNumber); // Output show karta hai .
 
-// conversionOperation :-
+// conversion :-
 // "33" => 33 Output // typeof - number
 // "33abc" => NaN Output // typeof - number 
 // true =>  Output 1; false => Output 0  // typeof - number
@@ -24,7 +24,7 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 console.log(typeof booleanIsLoggedIn)
 
-// conversionOperation :-
+// conversion :-
 // 1 => Output true;  0 => Output false // typeof - Boolean 
 // " " => Output false //  Boolean
 // "Shivani" => Output true // Boolean
@@ -36,7 +36,7 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
-// conversionOperation :-
+// conversion :-
 // 33 => Output 33 // typeof - String
 
 
@@ -45,3 +45,46 @@ console.log(typeof stringNumber);
 // Number ko String mei kaise convert kare And   // let stringNumber = String(someNumber)
 // String ko number mein conversion   // let valueInNumber = Number(score)
 // String ko boolean mein conversion  // let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+
+
+
+//******************************************* Operations **********************************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
