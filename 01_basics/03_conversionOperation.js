64a979f7c1c2036eb4c21d148fef466ce48cb913
Shivani Singh <150514536+shivani764300@users.noreply.github.com>
2026-07-10 02:20:47 +0000

    coversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,28 +1,34 @@
 // 3rd Topic - ConversionOperation : 
-
-let score = 33 
-
-console.log(typeof score);
-console.log(typeof(score));
-
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-
-// conversion :-
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
+// NaN (Not a Number)
 
 
-let isLoggedIn = "Shivani"
+let score = "33"// typeof - String hoga
+            // if let score = 33 // typeof - number  
+
+console.log(typeof score); // ye typeof check karne ko hai jaise ki upar mein value number hai ya koi others ?
+console.log(typeof(score)); // Aise bhi check kar sakte hai . yaha par as a method (score) check kiya hai .
+
+let valueInNumber = Number(score) // ye "33" is String ko --> number change ke liye ValueInNmuber use hota hai jise ham conversion kahte hai .
+console.log(typeof valueInNumber); // ab ham iska use kar ke  - jo value ko conversion kiya hai uska typeof check karte hai  Example - let spoose - "33abc" ye ek String hai but isko ham number mein convert kiye hai .
+console.log(valueInNumber); // Output show karta hai .
+
+// conversionOperation :-
+// "33" => 33 Output // typeof - number
+// "33abc" => NaN Output // typeof - number 
+// true =>  Output 1; false => Output 0  // typeof - number
+// "shivani" => Output NaN // typeof - number
+
+let isLoggedIn = "shivani"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn)
 
-// 1 => true;  0 => false 
-// " " => false
-// "Shivani" => true
+// conversionOperation :-
+// 1 => Output true;  0 => Output false // typeof - Boolean 
+// " " => Output false //  Boolean
+// "Shivani" => Output true // Boolean
+// 33 => Output true // Boolean
 
 let someNumber = 33
 
@@ -30,5 +36,12 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
-// 33 => string
+// conversionOperation :-
+// 33 => Output 33 // typeof - String
 
+
+
+// Basically ConversionOperation mein yahi hota hai ki ham :
+// Number ko String mei kaise convert kare And   // let stringNumber = String(someNumber)
+// String ko number mein conversion   // let valueInNumber = Number(score)
+// String ko boolean mein conversion  // let booleanIsLoggedIn = Boolean(isLoggedIn)
