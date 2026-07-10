a5dbe686c7a1fffd1e20173e09656e15a8a40a36
Shivani Singh <150514536+shivani764300@users.noreply.github.com>
2026-07-10 10:53:41 +0000

    DataType Summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -15,9 +15,13 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id); // Symbol(123)
+console.log(anotherId); // Symbol(123)
 
-// const bigNumber = 3456543576654356754n
+console.log(id === anotherId); // Output false 
+
+
+const bigNumber = 3456543576654356754n
 
 
 
@@ -25,16 +29,22 @@ console.log(id === anotherId);
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+const heros = ["shaktiman", "naagraj", "doga"]; // this a Array 
 let myObj = {
     name: "hitesh",
     age: 22,
-}
+}  // this is a Object // iske andar ham String , number etc likh sakte hai.
 
 const myFunction = function(){
     console.log("Hello world");
-}
+}  // this is a Function
 
-console.log(typeof anotherId);
+console.log(typeof bigNumber); // Jab DataType pata karna ho kisi value ka tab ham typeof use karte hai.
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// console.log(typeof anotherId); // Symbol
+//console.log(typeof Null); // object
+//console.log(typeof function); // function
+// console.log(typeof object); // object
+// console.log(typeof bigNumber); // BigInt
